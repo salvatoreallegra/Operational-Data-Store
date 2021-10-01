6efd9ee69ddc159b7113a6ec8341d0d0e4274439
Salvatore Allegra <salallegra@gmail.com>
2021-10-01 06:25:50 -0700

    Change to matchid

--- DBServices/MortalitySlopeDBService.cs
+++ DBServices/MortalitySlopeDBService.cs
@@ -19,7 +19,7 @@ namespace ODSApi.Services
         }
         public async Task AddAsync(MortalitySlopeEntity item)
         {
-            await _container.CreateItemAsync(item, new PartitionKey(item.Id));
+            await _container.CreateItemAsync(item, new PartitionKey(item.MatchId));
         }
 
         public async Task<MortalitySlopeEntity> GetAsync(string id)
